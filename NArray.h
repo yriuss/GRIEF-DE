026d7d255c9c61818efc70b58313737beddac26e
adriel <adriel.lins@ufpe.br>
2022-03-15 20:04:26 -0300

    last updates

--- NArray.h
+++ NArray.h
@@ -4,9 +4,9 @@
 #include <iostream>
 #include <vector>
 
-class Shape{
+class Array{
 public:
-    Shape(){
+    Array(){
         //allocate 2 elements
         ReAlloc(2);
     }
@@ -19,6 +19,10 @@ public:
         data[size++] = value;
     }
 
+    const Array& operator[](Array a) const{
+        return *this;
+    }
+
     const int operator[](size_t index) const{
         if(index > size){
             std::cout << "Invalid index!" << std::endl;
@@ -26,7 +30,6 @@ public:
         }
         return data[index];
     }
-
     //int operator[](size_t index){
     //    std::cout << "Not implemented yet!" << std::endl;
     //    exit(-1);
@@ -44,8 +47,8 @@ public:
         }
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Shape& instance){
-        os << "shape: [";
+    friend std::ostream& operator<<(std::ostream& os, const Array& instance){
+        os << "[";
         for(int i = 0; i < instance.size; i++){
             if(i < instance.size - 1)
                 os << instance.data[i] << ",";
@@ -56,6 +59,9 @@ public:
         return os;
     }
 
+    size_t length(){
+        return size;
+    }
 private:
     void ReAlloc(size_t newCapacity){
         //allocate a new block of memory
@@ -83,9 +89,59 @@ private:
 class NArray{
 public:
     NArray(std::vector<int> N);
-    Shape shape;
+    Array shape;
+    //const int operator[](size_t index) const{
+    //    if(index > size){
+    //        std::cout << "Invalid index!" << std::endl;
+    //        exit(-1);
+    //    }
+    //    return data[index];
+    //}
+
+    NArray& operator[](size_t index){
+        if(!wait_operation){
+            times_accessed++;
+
+            access += times_accessed*(index);
+            if(times_accessed == shape.length()){
+                times_accessed = 0;
+                wait_operation = true;
+            }
+        }
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, NArray& instance){
+        if(instance.times_accessed == 0){
+            os << instance.data[instance.access];
+        }
+        instance.access = 0;
+        instance.wait_operation = false;
+        return os;
+    }
+
+    void operator=(int element){
+        if(times_accessed != 0){
+            std::cout << "Invalid Operation" << std::endl;
+            exit(-1);
+        }
+        
+        data[access] = element;
+        access = 0;
+        wait_operation = false;
+        
+    }
+
+    void print(){
+        std::cout << data[0];
+    }
+
 private:
-    
+    int* data = nullptr;
+    size_t size = 1;
+    size_t access = 0;
+    size_t times_accessed = 0;
+    bool wait_operation = false;
 };
 
 #endif
