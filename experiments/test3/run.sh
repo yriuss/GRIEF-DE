8f306ae4b43b9465b58e0d38bdd6c3d343ee614a
Samuel Mendes <cicero.samuel.mendes@gmail.com>
2022-06-16 15:25:50 -0300

    [more less one]-indvs

--- experiments/test3/run.sh
+++
@@ -1,8 +0,0 @@
-# git pull
-rm -rf build/*
-cd build
-cmake ..
-make -j$(nproc)
-# dataset > gen number > individuals number > experiments number > k value, cross value  
-./teste michigan 1000 10 5 10 0.8
-cd ..
